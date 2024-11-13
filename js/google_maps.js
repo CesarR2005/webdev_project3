5237309223e1680316b9f2045af9ee72719a65ea
CesarR2005 <cramirez10@hawk.iit.edu>
2024-11-12 19:26:13 -0600

    adding google maps page, media query for smaller screens, and js for map interaction

---
+++ js/google_maps.js
@@ -0,0 +1,36 @@
+function initMap() {
+  const iitTower = { lat: 41.8354, lng: -87.6278 };
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: iitTower,
+    mapTypeId: 'roadmap'
+  });
+
+  const marker = new google.maps.Marker({
+    position: iitTower,
+    map: map,
+    title: "IIT Tower"
+  });
+
+  const infoWindow = new google.maps.InfoWindow({
+    content: "<h3>IIT Tower</h3><p>Located in Chicago, IL</p>"
+  });
+
+  marker.addListener("click", function() {
+    infoWindow.open(map, marker);
+  });
+
+  // Additional features //
+  map.setOptions({
+    styles: [
+      { "elementType": "geometry", "stylers": [{ "color": "#212121" }] },
+      { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }
+    ]
+  });
+
+  map.setOptions({
+    zoomControl: true,
+    streetViewControl: true
+  });
+}
