7e91d9205d5392641a071dd4d4aa3d9aadfad24f
CesarR2005 <cramirez10@hawk.iit.edu>
2024-11-12 20:52:14 -0600

    Update google_maps.js

--- js/google_maps.js
+++ js/google_maps.js
@@ -1,37 +1,36 @@
 function initMap() {
-  const iitTower = { lat: 41.8354, lng: -87.6278 };  
+  const iitTower = { lat: 41.8354, lng: -87.6278 };
 
-  
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 16,
     center: iitTower,
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    mapTypeControl: false  
+    mapTypeId: 'satellite', 
+    styles: [
+      {
+        "elementType": "labels",
+        "stylers": [
+          { "visibility": "on" }
+        ]
+      }
+    ]
   });
 
- 
   const marker = new google.maps.Marker({
     position: iitTower,
     map: map,
     title: "IIT Tower"
   });
 
-
   const infoWindow = new google.maps.InfoWindow({
     content: "<h3>IIT Tower</h3><p>Located in Chicago, IL</p>"
   });
 
- 
   marker.addListener("click", function() {
     infoWindow.open(map, marker);
   });
 
-  // Map features
+  // Additional features //
   map.setOptions({
-    styles: [
-      { "elementType": "geometry", "stylers": [{ "color": "#212121" }] },
-      { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }
-    ],
     zoomControl: true,
     streetViewControl: true
   });
